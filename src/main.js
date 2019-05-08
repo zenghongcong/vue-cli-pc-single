837515a1e356c8eda5072fd703aa6b415af0f813
zenghongcong <zenghongcong@baogongyoucai.com>
2019-05-08 14:10:42 +0800

    添加reset css

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
 import router from "@/router";
 import store from "@/store";
-import {ajax, regex } from "@/utils";
+import { ajax, regex } from "@/utils";
 import App from "@/App.vue";
-import "@/element-ui"
+import "@/element-ui";
+import 'reset-css';
 import "@/App.styl";
 
 Vue.config.productionTip = false;
